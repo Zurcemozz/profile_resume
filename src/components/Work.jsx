0a03a69014474562519d796286e9dcc67eaef2cb
Zurcemozz <zurcemozz@gmail.com>
2022-06-06 14:36:58 +0800

    weather app added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import WeatherApp from '../assets/weather_app.png'
 import WorkImg from '../assets/workImg.jpeg'
 
 const Work = () => {
@@ -9,7 +10,7 @@ const Work = () => {
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-blue-600">
             Work
           </p>
           <p className="py-6">// Check out some of my recent work</p>
@@ -19,21 +20,21 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${WeatherApp})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+            <div className="opacity-0 text-center group-hover:opacity-100">
+              <span className="text-lg font-bold text-white tracking-wider">
+                Weather App using ReactJs + API
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://weather-app-three-olive.vercel.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Zurcemozz/weather-App/tree/main">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
